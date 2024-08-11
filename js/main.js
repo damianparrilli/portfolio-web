8341c2d4b17c31e8423b7b5a1fb345d739e486d9
Damián Parrilli <125889767+damiparrilli@users.noreply.github.com>
2024-08-11 02:51:12 -0300

    Some updates

--- js/main.js
+++ js/main.js
@@ -7,3 +7,22 @@ document.querySelectorAll('.nav-menu__buttons a').forEach(anchor => {
     });
   });
   
+  document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.nav-menu');
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('fade');
+          entry.target.classList.remove('fade-out');
+        } else {
+          entry.target.classList.add('fade-out');
+          entry.target.classList.remove('fade');
+        }
+      });
+    }, { threshold: [0.5] });
+
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+  });
