f8770381b882817e8876c0feef7fcb296234018e
Damián Parrilli <125889767+damiparrilli@users.noreply.github.com>
2024-08-13 23:03:07 -0300

    Updates, updates everywhere

--- js/main.js
+++ js/main.js
@@ -1,83 +1,96 @@
 document.querySelectorAll('.nav-menu__buttons a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    const sections = document.querySelectorAll('.nav-menu');
+});
 
-    const observer = new IntersectionObserver((entries) => {
+document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('.nav-menu');
+
+  const observerFade = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fade');
+        entry.target.classList.remove('fade-out');
+      } else {
+        entry.target.classList.add('fade-out');
+        entry.target.classList.remove('fade');
+      }
+    });
+  }, { threshold: [0.5] });
+
+  sections.forEach(section => {
+    observerFade.observe(section);
+  });
+
+  const container = document.querySelector('.contenido__projects');
+  const items = document.querySelectorAll('.project');
+  const prevBtn = document.getElementById('flecha-izq');
+  const nextBtn = document.getElementById('flecha-der');
+
+  let currentIndex = 0;
+
+  const observerArrow = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        currentIndex = Array.from(items).indexOf(entry.target);
+        updateButtons();
+      }
+    });
+  }, { threshold: 0.5, root: container });
+
+  items.forEach(item => {
+    observerArrow.observe(item);
+  });
+
+  function updateButtons() {
+    prevBtn.style.display = currentIndex === 0 ? 'none' : 'block';
+    nextBtn.style.display = currentIndex === items.length - 1 ? 'none' : 'block';
+  }
+
+  function scrollToIndex(index) {
+    container.scrollLeft = index * container.offsetWidth;
+  }
+
+  prevBtn.addEventListener('click', () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+      scrollToIndex(currentIndex);
+      updateButtons();
+    }
+  });
+
+  nextBtn.addEventListener('click', () => {
+    if (currentIndex < items.length - 1) {
+      currentIndex++;
+      scrollToIndex(currentIndex);
+      updateButtons();
+    }
+  });
+
+  updateButtons();
+
+  if (window.innerWidth <= 900) {
+    const back = document.getElementById('back');
+
+    // Crear un nuevo observador de intersección
+    const observerAbout = new IntersectionObserver(entries => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('fade');
-          entry.target.classList.remove('fade-out');
+          back.classList.add('active');
         } else {
-          entry.target.classList.add('fade-out');
-          entry.target.classList.remove('fade');
+          back.classList.remove('active');
         }
       });
-    }, { threshold: [0.5] });
-
-    sections.forEach(section => {
-      observer.observe(section);
+    }, {
+      threshold: 0.1  // Ajusta este valor según la visibilidad deseada
     });
-  });
 
-    
-    
-    
-    
+    // Observar el elemento
+    observerAbout.observe(back);
+  }
 
-    document.addEventListener('DOMContentLoaded', () => {
-      const container = document.querySelector('.contenido__projects');
-      const items = document.querySelectorAll('.project');
-      const prevBtn = document.getElementById('flecha-izq');
-      const nextBtn = document.getElementById('flecha-der');
-    
-      let currentIndex = 0;
-    
-      const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            currentIndex = Array.from(items).indexOf(entry.target);
-            updateButtons();
-          }
-        });
-      }, { threshold: 0.5, root: container });
-    
-      items.forEach(item => {
-        observer.observe(item);
-      });
-    
-      function updateButtons() {
-        prevBtn.style.display = currentIndex === 0 ? 'none' : 'block';
-        nextBtn.style.display = currentIndex === items.length - 1 ? 'none' : 'block';
-      }
-    
-      function scrollToIndex(index) {
-        container.scrollLeft = index * container.offsetWidth;
-      }
-    
-      prevBtn.addEventListener('click', () => {
-        if (currentIndex > 0) {
-          currentIndex--;
-          scrollToIndex(currentIndex);
-          updateButtons();
-        }
-      });
-    
-      nextBtn.addEventListener('click', () => {
-        if (currentIndex < items.length - 1) {
-          currentIndex++;
-          scrollToIndex(currentIndex);
-          updateButtons();
-        }
-      });
-    
-      updateButtons(); // Initial call to set button visibility
-    });
-    
+});
